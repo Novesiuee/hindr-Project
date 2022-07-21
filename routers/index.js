6b764909a5bbbe47ed7b0f34278c488d6f5ba3dd
Novesiuee <aurorsenior@gmail.com>
2022-07-21 13:39:42 +0700

    Association

--- routers/index.js
+++ routers/index.js
@@ -7,7 +7,17 @@ router.get('/register', Controller.formRegister)
 router.post('/register', Controller.postRegister)
 
 //login form
+router.use(function (req, res, next){
+    next()
+})
+
 router.get('/login', Controller.formLogin)
 router.post('/login', Controller.postLogin)
 
+
+//admin endpoint
+
+
+
+//user endpoint
 module.exports = router
